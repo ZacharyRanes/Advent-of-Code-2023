5a4f7f147d85628346205bd155093a25edd491cc
Zachary Ranes <zachary.ranes@outlook.com>
2023-12-05 09:25:16 -0600

    part1 done

--- Day05/part1.py
+++ Day05/part1.py
@@ -13,33 +13,41 @@ levels = []
 
 while True:
 
-    mapped = {}
+    mapped = []
 
     while input_array[index][0].isdigit():
         work = list(map(int, input_array[index].split()))
-        for i in range(work[2]):
-            mapped[work[1]+i] = work[0]+i
+        mapped.append((work[1], work[1]+work[2], work[0]-work[1]))
 
         index += 1
         if index >= len(input_array):
             break
 
-    # levels.append(mapped)
+    levels.append(mapped)
 
-    print(mapped)
+    # print(mapped)
 
     index += 2
 
     if index >= len(input_array):
         break
 
-# for level in levels:
-#     path = []
-#     for s in seeds:
-#         if s in level:
-#             path.append(level[s])
-#         else:
-#             path.append(s)
-#     seeds = path
+# print(levels)
 
-# print(min(seeds))
+for level in levels:
+    path = []
+    for s in seeds:
+        found = False
+        print(level)
+        for m in level:
+            if s >= m[0] and s <= m[1]:
+                path.append(s + m[2])
+                found = True
+                break
+        if not found:
+            path.append(s)
+    # print(path)
+
+    seeds = path
+
+print(min(seeds))
