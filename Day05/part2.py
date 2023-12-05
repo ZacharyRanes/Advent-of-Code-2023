c7441bb538299766e4f7ef3d7240b4f0c3057fdb
Zachary Ranes <zachary.ranes@outlook.com>
2023-12-05 10:38:59 -0600

    part 2 not done

---
+++ Day05/part2.py
@@ -0,0 +1,52 @@
+input_array = []
+
+with open('input', 'r', encoding="UTF-8") as file_name:
+    input_array = file_name.readlines()
+
+seeds_list = list(map(int, input_array[0].split()[1:]))
+seeds = []
+for i in range(0, len(seeds_list)-1, 2):
+    seeds.append((seeds_list[i], seeds_list[i] + seeds_list[i+1]))
+
+index = 3
+
+levels = []
+
+while True:
+
+    mapped = []
+
+    while input_array[index][0].isdigit():
+        work = list(map(int, input_array[index].split()))
+        mapped.append((work[1], work[1]+work[2], work[0]-work[1]))
+
+        index += 1
+        if index >= len(input_array):
+            break
+
+    levels.append(mapped)
+
+    index += 2
+
+    if index >= len(input_array):
+        break
+
+# print(seeds)
+# print(levels)
+
+# small = -1
+
+# for r in seeds:
+#     for i in range(r[0], r[1]):
+#         found = False
+#         for m in mappings:
+#             if i >= m[0] and i <= m[1]:
+#                 i = (i + m[2])
+#                 found = True
+#                 break
+#             # if not found:
+#             #     i = i
+#         if i < small or small == -1:
+#             small = i
+
+# print(small)
