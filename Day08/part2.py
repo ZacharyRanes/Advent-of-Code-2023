7c7d361626d7c71a3d9751a8bab48da97c58e319
Zachary Ranes <zachary.ranes@outlook.com>
2023-12-08 17:04:25 -0600

    part 2 works with copy pasta lcm function

--- Day08/part2.py
+++ Day08/part2.py
@@ -1,6 +1,6 @@
 input_array = []
 
-with open('input_test2', 'r', encoding="UTF-8") as file_name:
+with open('input', 'r', encoding="UTF-8") as file_name:
     input_array = file_name.readlines()
 
 route = input_array[0].strip()
@@ -9,26 +9,58 @@ path = {}
 for n in input_array:
     path[n.split()[0]] = (n.split()[2][1:-1], n.split()[3][:-1])
 
-current_nodes = []
+nodes = []
 
 for k in path:
     if k[2] == "A":
-        current_nodes.append(k)
+        nodes.append(k)
 
 route_index = 0
-steps = 0
-while any(node[2] != "Z" for node in current_nodes):
-    # print(current_nodes)
-    if route[route_index] == 'R':
-        for j in range(len(current_nodes)):
-            current_nodes[j] = path[current_nodes[j]][1]
+l = []
+
+for current_node in nodes:
+    steps = 0
+    while current_node[2] != "Z":
+        if route[route_index] == 'R':
+            # print(current_node, "next", path[current_node][1], "R")
+            current_node = path[current_node][1]
+        else:
+            # print(current_node, "next", path[current_node][0], "L")
+            current_node = path[current_node][0]
+        steps += 1
+
+        route_index += 1
+        if route_index >= len(route):
+            route_index = 0
+    l.append(steps)
+
+print("Find the lcd of these numbers", l)
+
+##### <copy pasta>
+
+def find_lcm(num1, num2):
+    if(num1>num2):
+        num = num1
+        den = num2
     else:
-        for j in range(len(current_nodes)):
-            current_nodes[j] = path[current_nodes[j]][0]
-    steps += 1
+        num = num2
+        den = num1
+    rem = num % den
+    while(rem != 0):
+        num = den
+        den = rem
+        rem = num % den
+    gcd = den
+    lcm = int(int(num1 * num2)/int(gcd))
+    return lcm
 
-    route_index += 1
-    if route_index >= len(route):
-        route_index = 0
+num1 = l[0]
+num2 = l[1]
+lcm = find_lcm(num1, num2)
 
-print(steps)
+for i in range(2, len(l)):
+    lcm = find_lcm(lcm, l[i])
+
+print(lcm)
+
+#### </copy pasta>
