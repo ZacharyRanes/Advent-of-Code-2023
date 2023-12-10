031e53f9d476233439e014ab27e616d6d2024525
Zachary Ranes <zachary.ranes@outlook.com>
2023-12-09 22:30:56 -0600

    template day for easy of start of days

---
+++ Day00/part1.py
@@ -0,0 +1,14 @@
+def main():
+    total = 0
+    input_array = []
+
+    with open('input_test', 'r', encoding="UTF-8") as file_name:
+        input_array = file_name.readlines()
+
+    # Coder Here
+
+    return total
+
+
+if __name__ == "__main__":
+    print(main())
